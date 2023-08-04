4f5adb20ea86dc2505cae3b6cc3d54a9960d0685
euna2000 <eunakoh2000@gmail.com>
2023-08-04 23:58:53 +0900

    init commit

---
+++ src/main/java/com/sparta/springlv2/Springlv2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springlv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/springlv2/Springlv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springlv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springlv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
